196c8a654f873fff7d1ad6d1c5bb91acfa9dd79b
Gabriel Valluis <gvalluis@gmail.com>
2022-03-18 08:14:24 -0300

    Multiple selector

---
+++ src/forms/MultipleSelector.tsx
@@ -0,0 +1,69 @@
+import './MultipleSelector.css'
+
+
+
+export default function MultipleSelector(props: MultipleSelectorProps) {
+
+    function select(item: multipleSelectorModel) {
+        const selected = [...props.selected, item];
+        const nonSelected = props.nonSelected.filter(value => value !== item);
+        props.onChange(selected, nonSelected);
+    }
+
+    function deSelect(item: multipleSelectorModel) {
+        const selected = [...props.nonSelected, item];
+        const nonSelected = props.selected.filter(value => value !== item);
+        props.onChange(selected, nonSelected);
+    }
+
+    function selectAll() {
+        const selected = [...props.selected, ...props.nonSelected];
+        const nonSelected: multipleSelectorModel[] = [];
+        props.onChange(selected, nonSelected);
+    }
+
+    function deselectAll() {
+        const nonSelected = [...props.nonSelected, ...props.selected];
+        const selected: multipleSelectorModel[] = [];
+        props.onChange(selected, nonSelected);
+    }
+
+    return (
+        <div className="mb-3">
+            <div className="multiple-selector">
+                <label>{props.displayName}</label>
+                <ul>
+                    {
+                        props.nonSelected.map(item =>
+                            <li key={item.key} onChange={() => select(item)}>{item.value}</li>
+                        )
+                    }
+                </ul>
+                <div className="multiple-selector-buttons">
+                    <button type="button" onClick={selectAll}>{'>>'}</button>
+                    <button type="button" onClick={deselectAll}>{'<<'}</button>
+                </div>
+                <ul>
+                    {
+                        props.selected.map(item =>
+                            <li key={item.key} onChange={() => deSelect(item)}>{item.value}</li>
+                        )
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+interface MultipleSelectorProps {
+    displayName: string;
+    selected: Array<multipleSelectorModel>;
+    nonSelected: Array<multipleSelectorModel>;
+    onChange(selected: multipleSelectorModel[], nonSelected: multipleSelectorModel[])
+}
+
+export interface multipleSelectorModel {
+    key: number;
+    value: string;
+}
