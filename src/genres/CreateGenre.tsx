5890cfbf1f85bc529eb1fed42fd00673eba6865e
Gabriel Valluis <gvalluis@gmail.com>
2023-03-21 19:37:16 -0300

    API Integration; envs;

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,17 +1,30 @@
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { urlGenres } from '../utils/utils';
 import GenreForm from './GenreForm';
+import genreCreationDTO from './genres.model';
 
 export default function CreateGenre() {
+
+    async function create(genre: genreCreationDTO) {
+        const history = useHistory();
+        try {
+            await axios.post(urlGenres, genre);
+            history.push('/genres')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <h3>Create Genre</h3>
-            <GenreForm model={{name: ''}} 
-            onSubmit={
-                async value => {
-                    //when the form is posted
-                    await new Promise(r => setTimeout(r, 3000));
-                    console.log(value);
+            <GenreForm model={{ name: '' }}
+                onSubmit={
+                    async value => {
+                        await create(value)
+                    }
                 }
-            }
             />
         </>
     )
